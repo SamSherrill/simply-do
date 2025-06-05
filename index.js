33953ba5eac2dc4ce565cc3529fcaf6cf5f6c4cc
Sam Sherrill <38333695+SamSherrill@users.noreply.github.com>
2025-06-05 16:08:27 -0600

    Local DynamoDB initial setup

--- index.js
+++ index.js
@@ -1,9 +1,36 @@
-const express = require('express');
+const express = require("express");
+const AWS = require("aws-sdk");
 const app = express();
-const port = 3000; // You can choose a different port if you like
+const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+AWS.config.update({
+  region: 'us-west-2', // You can choose any dummy region for local
+  endpoint: 'http://localhost:8000'
+});
+
+AWS.config.credentials = {
+  accessKeyId: 'dummyKeyId',
+  secretAccessKey: 'dummySecretKey',
+};
+
+// Create a DynamoDB DocumentClient, which simplifies working with DynamoDB items
+const dynamodb = new AWS.DynamoDB.DocumentClient();
+const ddb = new AWS.DynamoDB();
+
+console.log("DynamoDB DocumentClient created"); // You can add this line to verify
+
+app.get("/", (req, res) => {
+  console.log("Root path '/' was accessed!");
+  res.send("Hello World!");
+});
+
+// Example: Listing DynamoDB tables
+ddb.listTables({}, (err, data) => {
+  if (err) {
+    console.error("Error listing tables:", err);
+  } else {
+    console.log("List of tables:", data.TableNames);
+  }
 });
 
 app.listen(port, () => {
